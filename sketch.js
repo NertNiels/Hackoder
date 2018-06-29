6a4fb80dbc091152148237fbc0e069d86ad2e029
Niels Smit <nertniels@gmail.com>
2018-06-29 19:06:22 +0200

    Made Project

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+}
+
+function draw() {
+  background(128,255,128)
+}
+
+window.onresize = function(event) {
+  resizeCanvas(windowWidth,windowHeight)
+}
