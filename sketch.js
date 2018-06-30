a1f8c379f1e59b02a830af0c0c47f2e7d1306815
Niels Smit <nertniels@gmail.com>
2018-06-30 12:50:00 +0200

    Made the first steps of the terminal!

--- sketch.js
+++ sketch.js
@@ -1,12 +1,44 @@
+var font;
+
+function preload() {
+  font = loadFont("assets/Terminal 1.ttf");
+}
+
+var terminal = "";
 
 function setup() {
-  createCanvas(windowWidth, windowHeight)
+  createCanvas(windowWidth, windowHeight);
+  writeLine("Starting hackOS...");
+  writeLine("hackOS has successfully started!")
 }
 
 function draw() {
-  background(128,255,128)
+  background(0, 0, 50);
+  noStroke();
+  fill(255);
+  textSize(16);
+  textAlign(LEFT, TOP);
+  textFont(font);
+  text(terminal, 30, 30, 400, 400);
+
+  noFill();
+  stroke(255);
+  rect(30, 30, 800, 800);
+
 }
 
 window.onresize = function(event) {
-  resizeCanvas(windowWidth,windowHeight)
+  resizeCanvas(windowWidth,windowHeight);
+}
+
+function writeLine(text) {
+  terminal += text + "\n";
+}
+
+function write(text) {
+  terminal += text;
+}
+
+function keyTyped() {
+  write(key);
 }
